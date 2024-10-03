b07e38cb6ba89e9224a6d3e8b58f290ff84bf5b6
Adrians Raitums <126756100+bigboy3302@users.noreply.github.com>
2024-10-03 13:20:53 +0300

    ggs

--- App/views/auth/login.view.php
+++ App/views/auth/login.view.php
@@ -12,13 +12,25 @@ require "../App/views/components/head.php";
                     <input class="auth-input" type="text" name="username" value="<?= $_POST["username"] ?? "" ?>"/>
                 </label>
                 <br><br>
-                <label class="auth-label" style="color:white">
+                
+                <!-- Password input with show/hide button inside -->
+                <label class="auth-label" style="color:white; position: relative;">
                     Password
-                    <input class="auth-input" type="password" name="password" value="<?= $_POST["password"] ?? "" ?>"/>
+                    <input class="auth-input" type="password" id="password" name="password" value="<?= $_POST["password"] ?? "" ?>" style="padding-right: 50px;" />
+                    
+                    <!-- Button inside password field -->
+                    <button type="button" class="btn-class-name" onclick="togglePassword()" style="position: absolute; right: 10px; top: 50%; transform: translateY(-50%);">
+                        <span class="back"></span>
+                        <span class="front">Show</span>
+                    </button>
                 </label>
+                <br><br>
+                
+                <!-- Display password error if exists -->
                 <?php if (isset($errors["password"])) { ?>
                     <p class="invalid-data"> <?= $errors["password"] ?> </p>
                 <?php } ?>
+                
                 <button class="auth-button">Submit</button>
             </form>
             <a href="/register">Register</a>
@@ -29,6 +41,18 @@ require "../App/views/components/head.php";
     </main>
 </form>
 
+<script>
+function togglePassword() {
+    var passwordField = document.getElementById("password");
+    var frontText = document.querySelector(".btn-class-name .front");
+    if (passwordField.type === "password") {
+        passwordField.type = "text";
+        frontText.textContent = "Hide"; // Change button text to "Hide" when password is shown
+    } else {
+        passwordField.type = "password";
+        frontText.textContent = "Show"; // Change back to "Show" when password is hidden
+    }
+}
+</script>
+
 <?php require "../App/views/components/footer.php" ?>
-
-
